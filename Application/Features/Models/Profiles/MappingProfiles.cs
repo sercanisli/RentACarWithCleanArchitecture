35ae1277b3262b9a4ee4e029e048f6a850992957
Sercan ISLI <sercanislii@gmail.com>
2023-08-14 02:07:22 +0300

    Query, Controller and Mapping Profiles were created for the Model entity.

---
+++ Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Queries.GetList;
+using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistance.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Model,GetListModelListItemDto>().ReverseMap();
+            CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+        }
+    }
+}
---
+++ WebApi/Controllers/ModelsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Queries.GetList;
+using Core.Application.Request;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new() { PageRequest = pageRequest };
+            GetListResponse<GetListModelListItemDto> response = await Mediator.Send(getListModelQuery);
+            return Ok(response);
+        }
+    }
+}
